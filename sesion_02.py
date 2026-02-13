34c20f1b2396bcc52c50c2bfb03be179809d4398
Gutiérrez-Jostin <elenamarquez2000casilla@gmail.com>
2026-02-12 18:58:44 -0800

    Guardando a mi sesion en python_basico_02

--- sesion_02.py
+++ sesion_02.py
@@ -7,3 +7,14 @@ mi_lista = [1, 3.1416, "Jostin", mi_tupla]
 print("El primer elemento de mi lista: ", mi_lista[0])
 print("El primer elemento de mi lista: ", mi_lista[3])
 print("El primer elemento de mi lista: ", mi_lista[2])
+
+#Diccionarios
+mi_diccionario = {
+    "mi_lista": mi_lista,
+    "nombre": "Jostin",
+    "pi": 3.1416,
+    "Tel": "663-1182238"
+}
+print("Llave para acceder a mi diccionario mi_lista", mi_diccionario["mi_lista"])
+print("Llave para acceder a mi diccionario pi", mi_diccionario["pi"])
+print("Llave para acceder a mi diccionario tel", mi_diccionario["Tel"])
