5d6ba5668f8e315adc24f5e2e374e305cc067a52
Gutierrez-Jostin <elenamarquez2000casilla@gmail.com>
2026-02-27 15:40:12 -0800

    Cree un ataque especial

--- OOP/Ogro.py
+++ OOP/Ogro.py
@@ -1,4 +1,5 @@
 from Enemigo import *
+import random
 
 class Ogro(Enemigo):
     def __init__(self,puntos_energia=20, ataque=3):
@@ -6,3 +7,10 @@ class Ogro(Enemigo):
 
     def habla(self):
         print("Ogro aplasta todo!!!")
+
+    def ataque_epecial(self):
+        print("Ogro ataque especial")
+        funciona_ataque_especial = random.random() < 0.20
+        if funciona_ataque_especial:
+            self.ataque += 4
+            print('Ogro enojado e incremento su atque por 4!!!')
